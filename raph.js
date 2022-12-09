07ce30e0e51ede62d904f7a86d6ec25b5395d9d3
huffybrown <tanurnimpa@gmail.com>
2022-12-09 02:26:34 -0800

    first upload

---
+++ raph.js
@@ -0,0 +1,17 @@
+let a=25
+let b=36
+function (a,b)
+let sum=a+b
+return (sum)
+
+
+function max ( 20,10){
+    if (20>10){
+        return 20;}
+}
+
+arr = { 1,2,3,4 50,60,100 } 
+int max = arr[0]; 
+for(int i=1; i<n ; i++) 
+if(arr[i]>max) 
+max = arr[i];
